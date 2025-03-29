dd196348a7ac7557a302ca44dc402ffb526b01c4
Adesef20 <musefiuadebisi@gmail.com>
2025-03-29 10:50:24 +0100

    i remove the btn again

--- app/page.js
+++ app/page.js
@@ -71,7 +71,6 @@ const Home = ()=> {
           <h2 className="text-3xl font-bold">Find Your Dream Car Today</h2>
           <p className="mt-4">Visits Jim-One Motors or contact us for the best deals.</p>
           <button className="mt-6 px-6 py-3 text-white bg-amber-500 font-bold rounded-lg shadow-md hover:bg-gray-500">Contact Us</button>
-          <button className="bg-slate-500 text-white rounded p-2">Nas</button>
         </section>
           <div>
           <Image src="https://res.cloudinary.com/dyp62yu3w/image/upload/v1743079739/image1_erbtgz.webp" alt="car" width={100} height={100}/>

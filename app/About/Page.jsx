5fd5048808aeb4e15a78c700b682d0fc188fe9d7
Adesef20 <musefiuadebisi@gmail.com>
2025-05-16 15:46:09 +0100

    Fix folder names to lowercase for routing

--- app/About/Page.jsx
+++ app/About/Page.jsx
@@ -1,13 +1,13 @@
 
 const About = () =>{
-    return(
-        <div className="p-16 text-center bg-white">
+   return(
+     <div className="p-16 text-center bg-white">
             <h2 className="text-3xl font-bold text-gray-800">About Jim-One Motors</h2>
-            <p className="w-3/4 text-gray-600 mx-auto mt-4">
+            <p className="w-3/4 mx-auto mt-4 text-gray-600">
              We provide high-quality vehicles, ensuring customer satisfaction and the best deals in the market with an excellent service.
             </p>
         </div>
-    );
+   );
 }
 
 export default About;
